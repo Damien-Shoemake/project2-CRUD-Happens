53b6c9ad1e2d0e8e43b7668d247dede5d2e2b97a
roodhouse <rughjm@gmail.com>
2022-11-07 21:28:08 -0600

    add api projectRoute controller

---
+++ controllers/api/projectRoute.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+const withAuth = require('../../utils/auth');
+require('dotenv').config();
+const request = require('request');
+
+router.get('/', async (req, res) => {
+  try {
+    request.get(
+      {
+        url: 'https://api.api-ninjas.com/v1/bucketlist',
+        headers: {
+          'X-Api-Key': process.env.API_KEY,
+        },
+      },
+      function (error, response, body) {
+        if (error) return console.error('Request failed:', error);
+        else if (response.statusCode != 200)
+          return console.error(
+            'Error:',
+            response.statusCode,
+            body.toString('utf8'),
+          );
+        else res.send(body);
+      },
+    );
+  } catch (err) {}
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const homeRoutes = require('./homeRoutes');
+const projectRoute = require('./api/projectRoute');
 
 router.use('/', homeRoutes);
+router.use('/api', projectRoute);
 
 module.exports = router;
