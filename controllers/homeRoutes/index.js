f5cd3ea3602f2199388690fdff73c9c8e33faf18
roodhouse <rughjm@gmail.com>
2022-11-10 13:07:29 -0600

    updated homeRoutes/index.js with signup and dashboard routes

--- controllers/homeRoutes/index.js
+++ controllers/homeRoutes/index.js
@@ -11,4 +11,12 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+});
+
 module.exports = router;
