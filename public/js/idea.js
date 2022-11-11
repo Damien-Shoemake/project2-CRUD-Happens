1f6d7a9ad226e4efe6bee1de0e2068e3592f1bd5
roodhouse <rughjm@gmail.com>
2022-11-11 13:04:32 -0600

    added code to idea function, attempt at toggle class

--- public/js/idea.js
+++ public/js/idea.js
@@ -2,7 +2,13 @@ const homeIdea = document.getElementById('homeIdea');
 
 function idea() {
     console.log('boom');
+    console.log(this);
+    // const apiSection = document.getElementById('apiSection');
+    $( this ).toggleClass( "show" );
+    // apiSection.toggleClass('show');
+    
 }
 homeIdea.addEventListener('click', idea);
 
 // place code from gunnner in slack here or inside idea()
+
