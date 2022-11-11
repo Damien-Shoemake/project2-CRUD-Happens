930d56a406a58cad227aeec506873678e8304c77
roodhouse <rughjm@gmail.com>
2022-11-11 13:15:40 -0600

    hidden div now appears on button click. boom.

--- public/js/idea.js
+++ public/js/idea.js
@@ -3,8 +3,10 @@ const homeIdea = document.getElementById('homeIdea');
 function idea() {
     console.log('boom');
     console.log(this);
+    const parent = $(this).parent().parent().parent().parent().siblings(".hidden");
+    console.log(parent);
     // const apiSection = document.getElementById('apiSection');
-    $( this ).toggleClass( "show" );
+    $( parent ).toggleClass( "show" );
     // apiSection.toggleClass('show');
     
 }
