08b3e9a56590e3e0ecac5ccc606ea6bc6d9cd71d
Daisy Le <daisy.le0203@gmail.com>
2022-11-10 22:11:18 -0600

    organized the controllers folder and added hyper link to the login and signup button

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 
 const homeRoutes = require('./homeRoutes');
-const projectRoute = require('./api/projectRoute');
-const apiRoutes = requrie('./api')
+const bucketlistRoutes = require('./bucketlistRoutes');
+const apiRoutes = require('./api')
 const dashboardRoutes = require('./dashboardRoutes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use("/bucketlist", bucketlistRoutes)
 
 module.exports = router;
