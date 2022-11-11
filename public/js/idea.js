3c259f2764fcbaff49ca0b2b029747292c907317
roodhouse <rughjm@gmail.com>
2022-11-11 14:43:45 -0600

    formatting

--- public/js/idea.js
+++ public/js/idea.js
@@ -1,13 +1,8 @@
 const homeIdea = document.getElementById('homeIdea');
 
 function idea() {
-    console.log('boom');
-    console.log(this);
     const parent = $(this).parent().parent().parent().parent().siblings(".hidden");
-    console.log(parent);
-    // const apiSection = document.getElementById('apiSection');
     $( parent ).toggleClass( "show" );
-    // apiSection.toggleClass('show');
     
 }
 homeIdea.addEventListener('click', idea);
