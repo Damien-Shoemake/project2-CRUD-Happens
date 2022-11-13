57889517063e5e5c51914181bcdacdce6154e022
roodhouse <rughjm@gmail.com>
2022-11-13 13:10:59 -0600

    added code to idea.js. it is not giving what we want at this time.

--- public/js/idea.js
+++ public/js/idea.js
@@ -1,11 +1,17 @@
 const homeIdea = document.getElementById('homeIdea');
 
-function idea() {
-    const parent = $(this).parent().parent().parent().parent().siblings(".hidden");
-    $( parent ).toggleClass( "show" );
+async function idea() {
+    const parent = $(this).parent().parent().parent().parent().siblings(".hidden"); // this code works
+    $( parent ).toggleClass( "show" ); // this code works
+
+    const response = await fetch ('/controllers/bucketlistRoutes', { // this code does not work
+        method: "GET"
+    });
+
+    console.log(response); // this code does something but not sure what is going on
     
 }
-homeIdea.addEventListener('click', idea);
+homeIdea.addEventListener('click', idea); // this code works
 
 // place code from gunnner in slack here or inside idea()
 
