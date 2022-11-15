a6fffb75893470119ea68fb0fefac0958d17d494
Daisy Le <daisy.le0203@gmail.com>
2022-11-15 12:44:39 -0600

    added the modal and more styling in the dashboard

--- public/js/idea.js
+++ public/js/idea.js
@@ -1,12 +1,8 @@
 const homeIdea = document.getElementById("homeIdea")
 
 async function idea() {
-  const parent = $(this).parent().parent().parent().parent().siblings(".hidden")
-  //   $(parent).toggleClass("show")
-  $(parent).removeClass("hidden")
-  $(parent).addClass("show")
 
-  const response = await fetch("/bucketlist", {
+   await fetch("/bucketlist", {
     method: "GET",
   })
     .then((result) => {
@@ -16,11 +12,7 @@ async function idea() {
       let title = document.getElementById("card-title")
       let ideaResult = (title.innerHTML = result.item)
 
-      console.log(result)
-      console.log(result.item)
-
       let addBucketItem = document.getElementById("addBucketItem")
-      console.log(addBucketItem)
 
       addBucketItem.onclick = () => {
         // add "ideaResult" to localStorage
