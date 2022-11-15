d9cbf3856a344061bc983cfdcfdbcc610e5e692b
Damien <Damientylershoemake@gmail.com>
2022-11-15 03:14:00 -0600

    started with markup, needs to be debugged. added a get 1 comment route

--- public/js/comment.js
+++ public/js/comment.js
@@ -29,6 +29,34 @@ async function commentFormHandler(event) {
   }
 }
 
-document
-  .querySelector("#comment-form")
-  .addEventListener("submit", commentFormHandler)
+ document
+   .querySelector("#comment-form")
+   .addEventListener("submit", commentFormHandler)
+
+
+  async function deleteFormHandler(event) {
+    event.preventDefault();
+  
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+  
+    const response = await fetch(`/api/comment/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+          id: id
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+  
+  }
+  
+  document.querySelector('#comment-form').addEventListener('click', deleteFormHandler);
