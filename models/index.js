26ca7468b6ce7894731709f0565bb96bd1e38049
Daisy Le <daisy.le0203@gmail.com>
2022-11-16 11:47:03 -0600

    cleaned up the code, styled the comments to match the rest of the page and delete Bucketlist model since it's not used

--- models/index.js
+++ models/index.js
@@ -1,18 +1,12 @@
 const User = require('./User');
 const Post = require('./Post')
 const Comment = require('./Comment')
-const Bucketlist = require('./Bucketlist')
-
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
 })
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id'
-})
-
-User.hasMany(Bucketlist, {
     foreignKey: 'user_id'
 })
 
@@ -32,8 +26,4 @@ Comment.belongsTo(Post, {
     foreignKey: 'post_id'
 })
 
-Bucketlist.belongsTo(User, {
-    foreignKey: 'user_id'
-})
-
-module.exports = { User, Post, Comment, Bucketlist };
+module.exports = { User, Post, Comment };
